67a312a6f1f78b189ce0eee9eb511eb0ede8af64
Iker <92671501+Ikerichu@users.noreply.github.com>
2025-09-23 22:12:33 +0000

    Excusa

--- src/app.js
+++ src/app.js
@@ -5,7 +5,16 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload=function excusa() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Generando una excusa");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = ["before the class","right on time","when I finished","during my lunch","while I was praying"];
+
+  let excuse =who[Math.floor(Math.random() * who.length)]+ " " +action[Math.floor(Math.random() * action.length)]+
+    " " +what[Math.floor(Math.random() * what.length)] + " " +when[Math.floor(Math.random() * when.length)];
+
+  document.getElementById("excuse").innerHTML = excuse;
 };
